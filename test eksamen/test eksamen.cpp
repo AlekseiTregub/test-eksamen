190cc4f18da135c08831152e80ceef002c47db28
AlekseiTregub <127427827+AlekseiTregub@users.noreply.github.com>
2023-05-17 13:49:29 +0300

    Добавьте файлы проекта.

---
+++ test eksamen/test eksamen.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream> 
+using namespace std;
+int main()
+{
+    setlocale(0, ".1251");
+    const int Nmax = 1000; 
+    double Arr[Nmax];
+    int n; 
+    double max;
+    int i_max; 
+    int i; 
+  
+    cout << "Ведіть розмір масиву ";
+ 
+    cin >> n;
+    cout << "Ведіть цілі цифри: ";
+ 
+    for (i = 0; i < n; i++)
+        cin >> Arr[i];
+    max = Arr[0];
+    i_max = 0;
+
+    for (i = 1; i < n; i++)
+        if (Arr[i] > max)
+        {
+            max = Arr[i];
+            i_max = i;
+        }
+
+    cout << "Arr[" << i_max << "] = " << max<<endl;
+    return 0;
+}
